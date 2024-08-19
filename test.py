0e5b30bc2c08ede8520e3a198aaa0e8d3faae249
HUANG POCHUN <52780588+avalanchechun@users.noreply.github.com>
2024-08-19 17:01:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,29 @@
-import xml.etree.ElementTree as ET
+import pyodbc
 import pandas as pd
-import io
 
-# The XML string you provided
-xml_string = """<ns:getTrxXmlResponse xmlns:ns="http://Test_Ws">ns:return<?xml version="1.0"?><transaction>..."""
+# 连接信息
+conn_str = (
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=your_server_name;"
+    "DATABASE=your_database_name;"
+    "UID=your_username;"
+    "PWD=your_password"
+)
 
-# Remove the namespace prefix and wrap the content in a root element
-xml_string = xml_string.replace('ns:', '')
-xml_string = '<root>' + xml_string + '</root>'
+# 建立连接
+conn = pyodbc.connect(conn_str)
 
-# Parse the XML
-tree = ET.fromstring(xml_string)
+# SQL 查询
+query = "SELECT * FROM your_table"
 
-# Find all 'oary' elements
-oary_elements = tree.findall('.//oary')
+# 读取查询结果到 DataFrame
+df = pd.read_sql(query, conn)
 
-# Create a list to store the data
-data = []
+# 关闭连接
+conn.close()
+
+# 显示 DataFrame 的前几行
+print(df.head())
+
+# DataFrame 基本信息
+print(df.info())
