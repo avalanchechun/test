6072642da27848041271f62e06cd330c1a2bf3ad
HUANG POCHUN <52780588+avalanchechun@users.noreply.github.com>
2024-08-26 18:23:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,40 +1,107 @@
-2024-08-26 18:15:37.631: 使用者資訊: IP=192.168.8.60, 用戶名=Unknown, 報告=LogSummary
-2024-08-26 18:15:37.631: 插入使用者資訊時發生錯誤: com.microsoft.sqlserver.jdbc.SQLServerDriver
-java.lang.ClassNotFoundException: com.microsoft.sqlserver.jdbc.SQLServerDriver
-	at org.apache.catalina.loader.WebappClassLoader.loadClass(WebappClassLoader.java:1340)
-	at org.apache.catalina.loader.WebappClassLoader.loadClass(WebappClassLoader.java:1189)
-	at org.apache.jasper.servlet.JasperLoader.loadClass(JasperLoader.java:148)
-	at org.apache.jasper.servlet.JasperLoader.loadClass(JasperLoader.java:69)
-	at java.lang.Class.forName0(Native Method)
-	at java.lang.Class.forName(Class.java:264)
-	at org.apache.jsp.LogSummary_jsp._jspService(LogSummary_jsp.java:407)
-	at org.apache.jasper.runtime.HttpJspBase.service(HttpJspBase.java:94)
-	at javax.servlet.http.HttpServlet.service(HttpServlet.java:802)
-	at org.apache.jasper.servlet.JspServletWrapper.service(JspServletWrapper.java:324)
-	at org.apache.jasper.servlet.JspServlet.serviceJspFile(JspServlet.java:292)
-	at org.apache.jasper.servlet.JspServlet.service(JspServlet.java:236)
-	at javax.servlet.http.HttpServlet.service(HttpServlet.java:802)
-	at org.apache.catalina.core.ApplicationFilterChain.internalDoFilter(ApplicationFilterChain.java:237)
-	at org.apache.catalina.core.ApplicationFilterChain.doFilter(ApplicationFilterChain.java:157)
-	at org.apache.catalina.core.StandardWrapperValve.invoke(StandardWrapperValve.java:214)
-	at org.apache.catalina.core.StandardValveContext.invokeNext(StandardValveContext.java:104)
-	at org.apache.catalina.core.StandardPipeline.invoke(StandardPipeline.java:520)
-	at org.apache.catalina.core.StandardContextValve.invokeInternal(StandardContextValve.java:198)
-	at org.apache.catalina.core.StandardContextValve.invoke(StandardContextValve.java:152)
-	at org.apache.catalina.core.StandardValveContext.invokeNext(StandardValveContext.java:104)
-	at org.apache.catalina.core.StandardPipeline.invoke(StandardPipeline.java:520)
-	at org.apache.catalina.core.StandardHostValve.invoke(StandardHostValve.java:137)
-	at org.apache.catalina.core.StandardValveContext.invokeNext(StandardValveContext.java:104)
-	at org.apache.catalina.valves.ErrorReportValve.invoke(ErrorReportValve.java:118)
-	at org.apache.catalina.core.StandardValveContext.invokeNext(StandardValveContext.java:102)
-	at org.apache.catalina.core.StandardPipeline.invoke(StandardPipeline.java:520)
-	at org.apache.catalina.core.StandardEngineValve.invoke(StandardEngineValve.java:109)
-	at org.apache.catalina.core.StandardValveContext.invokeNext(StandardValveContext.java:104)
-	at org.apache.catalina.core.StandardPipeline.invoke(StandardPipeline.java:520)
-	at org.apache.catalina.core.ContainerBase.invoke(ContainerBase.java:929)
-	at org.apache.coyote.tomcat5.CoyoteAdapter.service(CoyoteAdapter.java:160)
-	at org.apache.coyote.http11.Http11Processor.process(Http11Processor.java:799)
-	at org.apache.coyote.http11.Http11Protocol$Http11ConnectionHandler.processConnection(Http11Protocol.java:705)
-	at org.apache.tomcat.util.net.TcpWorkerThread.runIt(PoolTcpEndpoint.java:577)
-	at org.apache.tomcat.util.threads.ThreadPool$ControlRunnable.run(ThreadPool.java:683)
-	at java.lang.Thread.run(Thread.java:748)
+ 感謝您提供這個重要的信息。這確實解釋了為什麼系統找不到 SQL Server 的驅動程序。您使用的是 MySQL 的 JDBC 驅動，而不是 SQL Server 的驅動。讓我們根據這個情況來調整解決方案。
+
+
+
+以下是需要進行的修改：
+
+1. 更改 JDBC 驅動類：
+   將
+   ```java
+   Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+   ```
+   改為
+   ```java
+   Class.forName("com.mysql.jdbc.Driver");
+   ```
+
+2. 修改數據庫連接 URL：
+   將
+   ```java
+   String dbUrl = "jdbc:sqlserver://192.168.8.44;databaseName=WebPlatForm;encrypt=true;trustServerCertificate=true;";
+   ```
+   改為
+   ```java
+   String dbUrl = "jdbc:mysql://192.168.8.44:3306/WebPlatForm?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+   ```
+   注意：請確保端口號（這裡是 3306）正確，並根據需要調整 SSL 設置。
+
+3. 更新 SQL 語句：
+   MySQL 的語法可能與 SQL Server 略有不同。例如：
+   ```java
+   String insertSQL = "INSERT INTO WebPlatForm_Counter (IP, USER, REPORT, CLICK_TIME) VALUES (?, ?, ?, ?)";
+   ```
+
+4. 確保 MySQL 連接器在正確的位置：
+   驗證 `mysql-connector-java-5.1.18-bin.jar` 文件是否位於 Tomcat 的 `lib` 目錄或您的 Web 應用的 `WEB-INF/lib` 目錄中。
+
+5. 更新錯誤處理：
+   MySQL 的異常類可能與 SQL Server 的不同，確保您的 catch 塊能夠捕獲 MySQL 特定的異常。
+
+以下是修改後的代碼片段：
+
+```jsp
+<%
+    // ... (前面的代碼保持不變)
+
+    // 新增資料庫連接資訊
+    String dbUrl = "jdbc:mysql://192.168.8.44:3306/WebPlatForm?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+    String dbUser = "paulchun_huang";
+    String dbPassword = "Aa123456";
+
+    // 插入資料到 WebPlatForm_Counter 表格
+    String insertSQL = "INSERT INTO WebPlatForm_Counter (IP, USER, REPORT, CLICK_TIME) VALUES (?, ?, ?, ?)";
+    Connection newConn = null;
+    PreparedStatement pstmt = null;
+    boolean insertSuccess = false;
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+        logInfo("嘗試連接資料庫", logPath);
+        newConn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        logInfo("資料庫連接成功", logPath);
+        
+        pstmt = newConn.prepareStatement(insertSQL);
+        pstmt.setString(1, userIP);
+        pstmt.setString(2, userName);
+        pstmt.setString(3, reportName);
+        pstmt.setTimestamp(4, clickTime);
+        logInfo("準備執行SQL: " + insertSQL, logPath);
+        logInfo("參數: " + userIP + ", " + userName + ", " + reportName + ", " + clickTime, logPath);
+        
+        int affectedRows = pstmt.executeUpdate();
+        insertSuccess = (affectedRows > 0);
+        logInfo("SQL執行結果: 影響的行數 = " + affectedRows, logPath);
+    } catch (Exception e) {
+        String errorMessage = "插入使用者資訊時發生錯誤: " + e.getMessage();
+        logError(errorMessage, e, logPath);
+    } finally {
+        if (pstmt != null) {
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                logError("關閉 PreparedStatement 時發生錯誤", e, logPath);
+            }
+        }
+        if (newConn != null) {
+            try {
+                newConn.close();
+            } catch (SQLException e) {
+                logError("關閉資料庫連接時發生錯誤", e, logPath);
+            }
+        }
+    }
+
+    // ... (後面的代碼保持不變)
+%>
+
+```
+
+
+請使用這個更新後的代碼，然後再次運行您的 JSP 頁面。之後，請查看 `debug_log.txt` 文件以獲取更多信息。
+
+如果仍然遇到問題，請提供新的錯誤信息或日誌內容。另外，請確認以下幾點：
+
+1. MySQL 服務器是否在 192.168.8.44 上運行？
+2. 您是否有正確的用戶名和密碼來訪問 MySQL 數據庫？
+3. WebPlatForm 數據庫和 WebPlatForm_Counter 表是否存在於 MySQL 中？
+
+如果需要進一步的幫助或有任何疑問，請隨時告訴我。我們會繼續努力解決這個問題！
